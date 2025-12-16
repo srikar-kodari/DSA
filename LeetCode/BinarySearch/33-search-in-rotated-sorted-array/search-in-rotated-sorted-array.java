24eb1fa8c60391d77c23405a95dc8c85dcfd3d7e
Venkata Srikar Kodari <srikarkodari.dev@gmail.com>
2025-12-16 11:00:09 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (63.93%) - LeetSync

---
+++ LeetCode/BinarySearch/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int search(int[] arr, int target) {
+
+        int n = arr.length;
+        int low = 0;
+        int high = n-1;
+
+        while (low <= high) {
+            int mid = (low+high)/2;
+
+            if (arr[mid] == target) return mid;
+
+            //Left Sorted
+            if(arr[low] <= arr[mid]) {
+
+                if(arr[low] <= target && target <= arr[mid]) high = mid - 1;
+                else low = mid + 1;
+            }
+
+            //Right Sorted
+            else {
+
+                if(arr[mid] <= target && target <= arr[high]) low = mid + 1;
+                else high = mid - 1;
+            }
+            
+        }
+        return -1;
+        
+    }
+}
