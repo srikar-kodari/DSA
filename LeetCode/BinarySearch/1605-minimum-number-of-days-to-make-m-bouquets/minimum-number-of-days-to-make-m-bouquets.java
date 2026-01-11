8e9ad496328cbeecd6080138fd8ef31094541a67
Venkata Srikar Kodari <srikarkodari.dev@gmail.com>
2026-01-11 17:07:00 +0530

    Time: 29 ms (5.58%) | Memory: 82.8 MB (29.75%) - LeetSync

--- LeetCode/BinarySearch/1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ LeetCode/BinarySearch/1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,43 +1,40 @@
-// BINARY SEARCH
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
-        int n = bloomDay.length;
-        if((long) m*k > n) return -1;
 
-        int minEle = Arrays.stream(bloomDay).min().getAsInt();
-        int maxEle = Arrays.stream(bloomDay).max().getAsInt();
+        if(bloomDay.length < (m * k)) return -1;
 
-        int low = minEle;
-        int high = maxEle;
+        int low = Arrays.stream(bloomDay).min().getAsInt();
+        int high = Arrays.stream(bloomDay).max().getAsInt();
 
-        int result = -1;
+        int ans = -1;
 
         while(low <= high) {
             int mid = (low + high)/2;
 
-            if(possibleDay(bloomDay, mid, m, k)) {
-                result = mid;
+            if(isPossible(bloomDay, mid, m, k)) {
+                ans = mid;
                 high = mid - 1;
             }
             else low = mid + 1;
+
         }
-        return result;
+        return ans; //low is also the answer
     }
 
-    public boolean possibleDay(int bloomDay[], int day, int m, int k) {
-        
+    public static boolean isPossible(int[] bloomDay, int mid, int m, int k) {
         int count = 0;
-        int totalDays = 0;
+        int finalCount = 0;
 
-        for(int i=0; i<bloomDay.length; i++) {
-            if(bloomDay[i] <= day) count++;
+        for(int n : bloomDay) {
+            if(n <= mid) count++;
             else {
-                totalDays += count / k;
+                finalCount += count/k;
                 count = 0;
             }
         }
-        totalDays += count / k;
-        
-        return (totalDays >= m);
+        finalCount += count/k;
+
+        if(finalCount >= m) return true;
+        else return false;
     }
 }
