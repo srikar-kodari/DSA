ecf5dc8c852d185de38915a47fac12d5d18e1b9e
Venkata Srikar Kodari <srikarkodari.dev@gmail.com>
2026-01-11 15:52:05 +0530

    Time: 30 ms (6.31%) | Memory: 48 MB (44.01%) - LeetSync

---
+++ LeetCode/BinarySearch/907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+
+        int low = 1;
+        int high = Arrays.stream(piles).max().getAsInt();   //Max element in array
+
+        int ans  = Integer.MAX_VALUE;
+        while(low <= high) {
+            int mid = (low + high)/2;
+
+            int time = 0;
+            for(int n : piles) {
+                time += Math.ceil((double) n / (double) mid);   //ENSURE THE DIVISON TO BE DOUBLE
+            }
+
+            if(time <= h) {
+                
+                ans = Math.min(ans, mid);
+                high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+        return ans; //Answer is also = low
+        
+    }
+}
