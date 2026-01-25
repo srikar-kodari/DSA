aab874412aa266c5b7e419efd0767365a78e7dce
Venkata Srikar Kodari <srikarkodari.dev@gmail.com>
2026-01-25 11:23:44 +0530

    Time: 15 ms (43.61%) | Memory: 43.8 MB (65.83%) - LeetSync

---
+++ LeetCode/StringProblems/205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+
+        int len = s.length();
+        
+        Map<Character, Character> map = new HashMap<>();
+        Map<Character, Character> reverseMap = new HashMap<>();
+        
+        for(int i = 0; i < len; i++) {
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+            
+            if(!map.containsKey(sChar)) map.put(sChar, tChar);
+            else if(map.get(sChar) != tChar) return false;
+            
+            if(!reverseMap.containsKey(tChar)) reverseMap.put(tChar, sChar);
+            else if(reverseMap.get(tChar) != sChar) return false;
+
+        }   // The second if, else-if step ensures one-one mapping. Ex: s = "ab", t = "aa"
+
+        return true;
+    }
+}
