d86082c5114cbd6d12815279c41a3b7bcca56c4d
Venkata Srikar Kodari <srikarkodari.dev@gmail.com>
2026-01-26 18:07:40 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (94.62%) - LeetSync

---
+++ LeetCode/POTD/1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxDepth(String s) {
+
+        int count = 0;
+        int maxCount = 0;
+
+        char[] arr = s.toCharArray();
+
+        for(int i = 0; i<arr.length; i++) {
+
+            if(arr[i] == '(') {
+                count ++;
+                maxCount = Math.max(maxCount, count);
+            }
+            else if(arr[i] == ')') {
+                count--;
+            }
+        }
+
+        return maxCount;
+    }
+}
