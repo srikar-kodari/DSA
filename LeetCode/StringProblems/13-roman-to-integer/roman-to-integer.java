f80c6f5ab66cf360f14dfda18a5ff464d6f099be
Venkata Srikar Kodari <srikarkodari.dev@gmail.com>
2026-01-27 11:14:46 +0530

    Time: 4 ms (57.55%) | Memory: 46.8 MB (26.18%) - LeetSync

---
+++ LeetCode/StringProblems/13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int romanToInt(String s) {
+
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = 0;
+        
+        for(int i = 0; i < s.length(); i++) {
+
+            int current = map.get(s.charAt(i));
+
+            int next = (i + 1 < s.length()) ? map.get(s.charAt(i + 1)) : 0;
+            
+            if(current < next) {
+                result += next - current;
+                i++; // Skip next character
+            }
+            
+            else result += current;
+        }
+        
+        return result;
+    }
+}
