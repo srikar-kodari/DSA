64ed32c0617a6bf723625967465cba6460edbd3d
Venkata Srikar Kodari <srikarkodari.dev@gmail.com>
2026-02-13 16:05:14 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (70.10%) - LeetSync

---
+++ LeetCode/LinkedList/3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minimumOperations(int[] nums) {
+
+        int result = 0;
+        for(int x : nums) {
+            if(x % 3 != 0) result++;
+        }
+
+        return result;        
+    }
+}
