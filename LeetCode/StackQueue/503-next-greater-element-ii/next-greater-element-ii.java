b3c8d3db3d002a62ee2d3d5d9ce979998bdfeb86
Venkata Srikar Kodari <srikarkodari.dev@gmail.com>
2026-02-28 13:00:02 +0530

    Time: 15 ms (53.73%) | Memory: 48.8 MB (20.17%) - LeetSync

---
+++ LeetCode/StackQueue/503-next-greater-element-ii/next-greater-element-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        
+        Stack<Integer> stack = new Stack<>();
+        int n = nums.length;
+        int[] nGE = new int[n];
+
+        for(int i = 2*n-1; i >= 0; i--) {
+
+            while (!stack.isEmpty() && stack.peek() <= nums[i % n]) {
+                stack.pop();
+            }
+
+            if(i < n) {
+                nGE[i] = stack.isEmpty() ? -1 : stack.peek();
+            }
+            stack.push(nums[i % n]);
+        }
+
+        return nGE;
+    }
+}
