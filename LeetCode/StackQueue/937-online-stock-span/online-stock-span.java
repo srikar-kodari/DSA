18256cc90ba6dbdeb9200745c4336b41cae47d93
Venkata Srikar Kodari <srikarkodari.dev@gmail.com>
2026-03-03 14:02:09 +0530

    Time: 34 ms (20.32%) | Memory: 54.2 MB (84.17%) - LeetSync

---
+++ LeetCode/StackQueue/937-online-stock-span/online-stock-span.java
@@ -0,0 +1,31 @@
+class StockSpanner {
+
+    Stack<int[]> stack;
+    int index;
+
+    public StockSpanner() {
+        stack = new Stack<>();
+        index = -1;
+    }
+    
+    public int next(int price) {
+
+        index += 1;
+
+        while ((!stack.isEmpty()) && stack.peek()[0] <= price) {
+            stack.pop();
+        }
+
+        int ans = index - ((stack.isEmpty()) ? -1 : stack.peek()[1]);
+
+        stack.push(new int[] {price, index});
+
+        return ans;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
