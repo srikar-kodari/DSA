d182f11d3faa96654df0c8f5a3623d54adbac596
Venkata Srikar Kodari <srikarkodari.dev@gmail.com>
2026-03-07 15:08:40 +0530

    Time: 28 ms (39.10%) | Memory: 48.4 MB (50.49%) - LeetSync

---
+++ LeetCode/Heaps/621-task-scheduler/task-scheduler.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        
+        int[] freq = new int[26];
+        for(char ch : tasks) freq[ch - 'A']++;
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int f : freq) {
+            if(f > 0) maxHeap.add(f);
+        }
+
+        int time = 0;
+
+        while(!maxHeap.isEmpty()) {
+
+            List<Integer> temp = new ArrayList<>();
+            int cycle = n + 1;
+
+            while(cycle > 0 && !maxHeap.isEmpty()) {
+
+                int count = maxHeap.poll();
+                if(count - 1 > 0) temp.add(count - 1);
+
+                time++;
+                cycle--;
+            }
+
+            for(int t : temp) maxHeap.add(t);
+
+            if(maxHeap.isEmpty()) break;
+
+            time += cycle;
+        }
+
+        return time;
+    }
+}
