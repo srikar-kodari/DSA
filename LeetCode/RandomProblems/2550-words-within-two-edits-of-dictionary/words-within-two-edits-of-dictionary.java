fcd959506c0e5f308bf1f6e81e376e58cb4f6339
Venkata Srikar Kodari <srikarkodari.dev@gmail.com>
2026-04-22 13:51:37 +0530

    Time: 2 ms (97.79%) | Memory: 44.4 MB (50.00%) - LeetSync

---
+++ LeetCode/RandomProblems/2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<String> twoEditWords(String[] queries, String[] dictionary) {
+        List<String> result = new ArrayList<>();
+        
+        for (String q : queries) {
+            for (String d : dictionary) {
+                if (isWithinTwoEdits(q, d)) {
+                    result.add(q);
+                    break;
+                }
+            }
+        }
+        
+        return result;
+    }
+    
+    private boolean isWithinTwoEdits(String a, String b) {
+        int diff = 0;
+        
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                diff++;
+                if (diff > 2) return false;
+            }
+        }
+        
+        return true;
+    }
+}
