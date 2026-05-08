03694fd8b3d69036aa376c283d6a001e03722176
Venkata Srikar Kodari <srikarkodari.dev@gmail.com>
2026-05-08 22:51:49 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (57.47%) - LeetSync

---
+++ LeetCode/RandomProblems/3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+
+        int count = 0;
+        for (int num : nums) {
+            if (num < k) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
